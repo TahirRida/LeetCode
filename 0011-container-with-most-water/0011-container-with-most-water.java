071eb3813d2ec3ae046b6bc274a122b7f72b1970
TahirRida <tahirrida52@gmail.com>
2024-10-09 12:08:39 +0100

    Time: 4 ms (89.35%), Space: 58.1 MB (29.35%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,23 +1,21 @@
 class Solution {
-    public int maxArea(int[] height) {
-        int maxArea = 0; // To store the maximum area found
-        int left = 0; // Starting from the leftmost line
-        int right = height.length - 1; // Starting from the rightmost line
-
-        while (left < right) {
-            // Calculate the current area between left and right lines
-            int currentArea = Math.min(height[left], height[right]) * (right - left);
-            // Update maxArea if currentArea is larger
-            maxArea = Math.max(maxArea, currentArea);
-
-            // Move the pointer of the shorter line inward
-            if (height[left] < height[right]) {
+    public int maxArea(int[] heights) {
+        int left=0;int right=heights.length-1;
+        int water=0;
+        while(left<right){
+            int currentWater = (right-left)*Math.min(heights[left],heights[right]);
+            if(currentWater>water){
+                water = currentWater;
+            }
+            else if(heights[left] > heights[right]){
+                right --;
+            }
+            else{
                 left++;
-            } else {
-                right--;
             }
         }
+        return water;
 
-        return maxArea;
     }
 }
+
