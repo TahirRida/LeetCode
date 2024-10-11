78ecb8fdc8b8c10fc6dd27f251833c81d2cce6d5
TahirRida <tahirrida52@gmail.com>
2024-10-11 11:47:25 +0100

    Time: 0 ms (100.00%), Space: 46.2 MB (48.07%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,39 +1,23 @@
-class Solution {
-    public int trap(int[] heights) {
-        int left = 0;
-        int right = 1;
-        int water = 0;
-
-        while (left < right && right < heights.length) {
-            // Move 'right' to find a valid right boundary
-            while (right < heights.length && heights[right] < heights[left]) {
-                right++;
-            }
-
-            // If we reached the end of the array, break
-            if (right >= heights.length) {
-                break;
-            }
-
-            // Calculate the water trapped between left and right
-            for (int i = left + 1; i < right; i++) {
-                water += Math.max(0, Math.min(heights[left], heights[right]) - heights[i]);
-            }
-
-            // Move 'left' to the 'right' and increment 'right'
-            left = right++;
+public class Solution {
+    public int trap(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
         }
 
-        // Handling the case where 'right' goes out of bounds, but there is a plateau on the right
-        int maxHeight = heights[heights.length - 1];
-        for (int i = heights.length - 2; i >= left; i--) {
-            if (heights[i] < maxHeight) {
-                water += maxHeight - heights[i];
+        int l = 0, r = height.length - 1;
+        int leftMax = height[l], rightMax = height[r];
+        int res = 0;
+        while (l < r) {
+            if (leftMax < rightMax) {
+                l++;
+                leftMax = Math.max(leftMax, height[l]);
+                res += leftMax - height[l];
             } else {
-                maxHeight = heights[i];
+                r--;
+                rightMax = Math.max(rightMax, height[r]);
+                res += rightMax - height[r];
             }
         }
-
-        return water;
+        return res;
     }
 }
