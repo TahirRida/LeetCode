117958c3d0b650c942285469f19bcac14e647856
TahirRida <tahirrida52@gmail.com>
2024-10-12 18:29:43 +0100

    Time: 3 ms (12.01%), Space: 41.5 MB (36.38%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,20 +1,34 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<Character>(); // create an empty stack
-        for (char c : s.toCharArray()) { // loop through each character in the string
-            if (c == '(') // if the character is an opening parenthesis
-                stack.push(')'); // push the corresponding closing parenthesis onto the stack
-            else if (c == '{') // if the character is an opening brace
-                stack.push('}'); // push the corresponding closing brace onto the stack
-            else if (c == '[') // if the character is an opening bracket
-                stack.push(']'); // push the corresponding closing bracket onto the stack
-            else if (stack.isEmpty() || stack.pop() != c) // if the character is a closing bracket
-                // if the stack is empty (i.e., there is no matching opening bracket) or the top of the stack
-                // does not match the closing bracket, the string is not valid, so return false
-                return false;
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i<s.length();i++){
+            if(s.charAt(i)=='(' || s.charAt(i) == '{' || s.charAt(i)== '['){
+                st.push(s.charAt(i));
+            }
+            else{
+                if(st.empty()){
+                    return false;
+                }
+                if(s.charAt(i)==')'){
+                    if(st.pop()!='('){
+                        return false;
+                    }
+                }
+                if(s.charAt(i)==']'){
+                    if(st.pop()!='['){
+                        return false;
+                    }
+                }
+                if(s.charAt(i)=='}'){
+                    if(st.pop()!='{'){
+                        return false;
+                    }
+                }
+            }
         }
-        // if the stack is empty, all opening brackets have been matched with their corresponding closing brackets,
-        // so the string is valid, otherwise, there are unmatched opening brackets, so return false
-        return stack.isEmpty();
+        if(st.empty()){
+            return true;
+        }
+        return false;
     }
 }
