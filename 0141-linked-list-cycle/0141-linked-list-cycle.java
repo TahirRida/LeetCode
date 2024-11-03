7e7060e0444a96651df2d587cc11d1f245f0dc84
TahirRida <tahirrida52@gmail.com>
2024-11-03 14:46:29 +0100

    Time: 0 ms (100.00%), Space: 44.5 MB (39.69%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -9,16 +9,16 @@
  *     }
  * }
  */
-public class Solution {
+class Solution {
     public boolean hasCycle(ListNode head) {
-        HashMap<ListNode,Integer> map = new HashMap<>();
-        while(head != null){
-            if(!map.containsKey(head)){
-                map.put(head,1);
-                head = head.next;
-                continue;
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && slow != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow){
+                return true;
             }
-            return true;
         }
         return false;
     }
