b5f4e40fc54611c61a12fa6f3e272f74f1deedb2
TahirRida <tahirrida52@gmail.com>
2024-11-04 18:01:39 +0100

    Time: 0 ms (100.00%), Space: 42 MB (66.98%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,27 +1,33 @@
-class Solution {
+public class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int row = 0;
-        int low = 0;
-        int high = matrix[0].length -1;
-        while(matrix[row][high]<target){
-            if(row + 1 < matrix.length){
-                row++;
-            }
-            else{
-                return false;
+        int ROWS = matrix.length;
+        int COLS = matrix[0].length;
+
+        int top = 0, bot = ROWS - 1;
+        while (top <= bot) {
+            int row = (top + bot) / 2;
+            if (target > matrix[row][COLS - 1]) {
+                top = row + 1;
+            } else if (target < matrix[row][0]) {
+                bot = row - 1;
+            } else {
+                break;
             }
         }
-        int[] nums = matrix[row];
-        while(low <= high){
-            int mid = (low + high)/2;
-            if(nums[mid] == target){
+
+        if (!(top <= bot)) {
+            return false;
+        }
+        int row = (top + bot) / 2;
+        int l = 0, r = COLS - 1;
+        while (l <= r) {
+            int m = (l + r) / 2;
+            if (target > matrix[row][m]) {
+                l = m + 1;
+            } else if (target < matrix[row][m]) {
+                r = m - 1;
+            } else {
                 return true;
-            }
-            if(nums[mid]>target){
-                high = mid -1;
-            }
-            if(nums[mid]<target){
-                low = mid + 1;
             }
         }
         return false;
