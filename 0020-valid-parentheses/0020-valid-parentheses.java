7d1a4428bf39c297170045548af7727cfc550e5e
TahirRida <tahirrida52@gmail.com>
2024-11-06 18:35:05 +0100

    Time: 3 ms (44.87%), Space: 41.5 MB (48.86%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,34 +1,26 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st = new Stack<>();
+        Stack<Character> st = new Stack();
         for(int i = 0; i<s.length();i++){
-            if(s.charAt(i)=='(' || s.charAt(i) == '{' || s.charAt(i)== '['){
+            if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='['){
                 st.push(s.charAt(i));
             }
             else{
-                if(st.empty()){
+                if(st.isEmpty()){
                     return false;
                 }
-                if(s.charAt(i)==')'){
-                    if(st.pop()!='('){
-                        return false;
-                    }
+                char c = st.pop();
+                if(c=='(' && s.charAt(i)!=')'){
+                    return false;
                 }
-                if(s.charAt(i)==']'){
-                    if(st.pop()!='['){
-                        return false;
-                    }
+                if(c=='{' && s.charAt(i)!='}'){
+                    return false;
                 }
-                if(s.charAt(i)=='}'){
-                    if(st.pop()!='{'){
-                        return false;
-                    }
+                if(c=='[' && s.charAt(i)!=']'){
+                    return false;
                 }
             }
         }
-        if(st.empty()){
-            return true;
-        }
-        return false;
+        return st.isEmpty();
     }
 }
