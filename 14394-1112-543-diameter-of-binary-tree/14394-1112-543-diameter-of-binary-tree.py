fe7be8ffaf9a21bc4a4463c87faa9c342db1005c
TahirRida <tahirrida52@gmail.com>
2025-01-28 16:44:29 +0100

    Time: 5 ms, Memory: 20.8 MB - LeetHub

--- 14394-1112-543-diameter-of-binary-tree/14394-1112-543-diameter-of-binary-tree.py
+++ 14394-1112-543-diameter-of-binary-tree/14394-1112-543-diameter-of-binary-tree.py
@@ -7,21 +7,16 @@
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         self.max_diameter = 0
-        
-        def depth(node):
-            if not node:
+
+        def depth(root:Optional[TreeNode]) -> int:
+            if not root:
                 return 0
-            # Recursively find the depth of left and right subtrees
-            left_depth = depth(node.left)
-            right_depth = depth(node.right)
-            
-            # Update the maximum diameter
-            self.max_diameter = max(self.max_diameter, left_depth + right_depth)
-            
-            # Return the height of the current subtree
-            return 1 + max(left_depth, right_depth)
+            left_depth = depth(root.left)
+            right_depth = depth(root.right)
+            self.max_diameter = max(self.max_diameter,left_depth+right_depth)
+            return 1+max(left_depth,right_depth)
         
-        depth(root)  # Start the recursive depth calculation
+        depth(root)
         return self.max_diameter
         
         
